2688b65ae937d983447ebe70603023d2908a11ab
Elisa Shapiro <elisashapiro@gmail.com>
2021-08-19 22:16:59 -0400

    completed forked

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,12 @@
 class Magazine < ActiveRecord::Base
     has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        self.readers.map {|reader| reader.email}.join(";")
+    end
+    
+    def self.most_popular
+        self.all.sort{|a, b| a.subscriptions.length <=> b.subscriptions.length}.reverse[0]
+    end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,17 @@
 class Reader < ActiveRecord::Base
     has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        new_subscription = Subscription.create(price: price, magazine_id: magazine.id, reader_id: self.id)
+    end
+
+    def total_subscription_price
+        self.subscriptions.sum(&:price)
+    end
+
+    def cancel_subscription(magazine)
+        cancelme = Subscription.find_by(magazine_id: magazine.id)
+        cancelme.destroy
+    end
 end
